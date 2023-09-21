f0ac6fcfe65d5efc09a52bed59a092ecaa6333c7
zainulmustafa <zain2984@gmail.com>
2023-09-21 01:08:03 -0300

    map added

--- lmc/src/App.js
+++ lmc/src/App.js
@@ -1,31 +1,35 @@
 import lineString from "turf-linestring";
 import bezierSpline from "@turf/bezier-spline";
-import { featureCollection, point } from '@turf/helpers';
-import { coordEach } from '@turf/meta';
 import * as turf from '@turf/turf';
 import DATA from './assets/data/coordinates.json'
+import MapWidget from "./components/MapWidget";
 
-const dataset = DATA.datasets.dataset1
+// Extract data from the imported JSON file
+const dataset = DATA.datasets.dataset3
 const originalCoordinates = dataset.coordinates
 const controlledCoordinate = dataset.controller
 
+// Function to convert GeoJSON to a list of points
 function geoJsonToListOfPoints(geoJson = {}) {
   const pointList = [...(geoJson?.geometry?.coordinates ?? [])]
   return pointList;
 }
 
+// Function to check if a coordinate is present in a list
 function isCoordinateInList(coordinate, coordinateList = []) {
   return coordinateList.some((item) => {
     return item[0] === coordinate[0] && item[1] === coordinate[1];
   });
 }
 
+// Function to find the index of a coordinate in a list
 function findIndexOfCoordinate(coordinate, coordinateList = []) {
   return coordinateList.findIndex((item) => {
     return item[0] === coordinate[0] && item[1] === coordinate[1];
   });
 }
 
+// Function to look up coordinates in a list
 function lookupCoordinatesInList(originalCoordinates = [], bigCoordinateList = []) {
   for (const coordinate of originalCoordinates) {
     if (isCoordinateInList(coordinate, bigCoordinateList)) {
@@ -36,17 +40,18 @@ function lookupCoordinatesInList(originalCoordinates = [], bigCoordinateList = [
   }
 }
 
+// Function to calculate the distance between two points using Turf.js
 function distanceCalculator(from, to) {
   var distance = turf.distance(from, to, { units: DATA.config.units });
-  // console.log(`Distance between ${from} and ${to} is ${distance}`)
   return distance
 }
 
+// Function to calculate the distance ratio between three points
 function distanceRatioCalculator(boundaryStart, center, boundaryEnd) {
-  // console.log(`Distance Ratio: ${distanceCalculator(boundaryStart, center)} / ${distanceCalculator(center, boundaryEnd)}`)
   return distanceCalculator(boundaryStart, center) / distanceCalculator(center, boundaryEnd)
 }
 
+// Function to find a coordinate with the minimum difference in ratio
 function findCoordinateWithMinDifference(coordinates, startIndex, ratioCalculator, targetRatios) {
   return coordinates
     .slice(startIndex)
@@ -63,23 +68,25 @@ function findCoordinateWithMinDifference(coordinates, startIndex, ratioCalculato
 }
 
 function App() {
+  // Calculate a curved path from original coordinates
   const curved = bezierSpline(lineString([...originalCoordinates]));
   const listCurvedCoordinates = geoJsonToListOfPoints(curved)
+
+  // Calculate original distance ratios
   const originalRatios = {
     first: distanceRatioCalculator(originalCoordinates.at(0), originalCoordinates.at(1), originalCoordinates.at(2)),
     second: distanceRatioCalculator(originalCoordinates.at(2), originalCoordinates.at(3), originalCoordinates.at(4))
   }
-  console.log(`Original ratios:`, originalRatios)
 
-  // adjusting the curved path
+  // Adjust the curved path by inserting a controlled coordinate
   const adjustedCoordinates = [originalCoordinates.at(0), controlledCoordinate, originalCoordinates.at(-1)]
   const adjustedCurved = bezierSpline(lineString([...adjustedCoordinates]), { resolution: DATA.config.precision });
   const listAdjustedCurvedCoordinates = geoJsonToListOfPoints(adjustedCurved)
 
+  // Find the index of the controlled coordinate in the adjusted list
   const controlledCoordinateIndexInInterpolatedList = findIndexOfCoordinate(controlledCoordinate, listAdjustedCurvedCoordinates)
-  // console.log(listAdjustedCurvedCoordinates.length, controlledCoordinateIndexInInterpolatedList)
 
-  // divide search
+  // Divide the search into two parts and find adjusted coordinates with minimum difference in ratio
   const firstAdjustedCoordinate = findCoordinateWithMinDifference(
     listAdjustedCurvedCoordinates,
     0,
@@ -96,12 +103,26 @@ function App() {
     originalRatios.second
   );
 
-  console.log(firstAdjustedCoordinate)
-  console.log(secondAdjustedCoordinate)
+  // Log the results
+  console.log("Original ratios:", originalRatios)
+  console.log("First adj:", firstAdjustedCoordinate)
+  console.log("Second adj:", secondAdjustedCoordinate)
+  const fullyAdjustedCoordinates = [adjustedCoordinates.at(0), firstAdjustedCoordinate.coordinate, controlledCoordinate, secondAdjustedCoordinate.coordinate, adjustedCoordinates.at(1)]
 
   return (
-    <div className="">
-      {JSON.stringify(curved)}
+    <div className="" style={{ height: '100vh', width: '100vw', backgroundColor: 'red' }}>
+      {/* {JSON.stringify(curved)} */}
+      <MapWidget
+        controlledCoordinate={controlledCoordinate}
+        originalCoordinates={{
+          list: listCurvedCoordinates,
+          points: originalCoordinates
+        }}
+        adjustedCoordinates={{
+          list: listAdjustedCurvedCoordinates,
+          points: fullyAdjustedCoordinates
+        }}
+      />
     </div>
   );
 }
